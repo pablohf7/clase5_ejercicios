3d4388045bff2a7d7d3cbfc33b6ab8a2ce2f4a0b
pablohf7 <pablohf7@gmail.com>
2026-05-16 12:58:47 -0500

    Agregado bucle while

--- compras/sistema_compras.py
+++ compras/sistema_compras.py
@@ -1,1 +1,14 @@
-print('Hola Mundo')
+while True:
+    print('Bievenidos al sistema de compras')
+    print('1=Entrar')
+    print('2=Salir')
+
+    opcion = input('Opcion: ')
+    if opcion == '1':
+        print('Entraste')
+    elif opcion == '2':
+        print('Saliendo')
+        break
+    else:
+        print('Reintenta')
+
