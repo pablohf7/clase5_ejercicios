d323321622b748be66e7e3fbe0a471ca9b87a587
pablohf7 <pablohf7@gmail.com>
2026-05-16 13:01:07 -0500

    Agregado de función menu

--- compras/sistema_compras.py
+++ compras/sistema_compras.py
@@ -1,14 +1,15 @@
-while True:
-    print('Bievenidos al sistema de compras')
-    print('1=Entrar')
-    print('2=Salir')
+def Menu():
+    while True:
+        print('Bievenidos al sistema de compras')
+        print('1=Entrar')
+        print('2=Salir')
 
-    opcion = input('Opcion: ')
-    if opcion == '1':
-        print('Entraste')
-    elif opcion == '2':
-        print('Saliendo')
-        break
-    else:
-        print('Reintenta')
+        opcion = input('Opcion: ')
+        if opcion == '1':
+            print('Entraste')
+        elif opcion == '2':
+            print('Saliendo')
+            break
+        else:
+            print('Reintenta')
 
